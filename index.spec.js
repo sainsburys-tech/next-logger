040804ef16e19bd9caccf51ced728cb8e6389102
Chris Atkin <atkinchris@gmail.com>
2021-04-28 19:23:54 +0100

    Add scripts to cover basic Next.js logger scenarios

--- index.spec.js
+++ index.spec.js
@@ -1,21 +1,91 @@
 const { exec } = require('child_process')
 const { promisify } = require('util')
+const os = require('os')
 
 const execAsync = promisify(exec)
 
-describe('next.js logger object', () => {
-  it('logs something', async () => {
-    const script = `
-      require('next/dist/build/output/log').error('Something went wrong')
-    `
-    const { stdout, stderr } = await execAsync(`node -r "." -e "${script}"`)
+const runScript = async (script) => {
+  const process = execAsync(`node -r "." -e "${script}"`)
+  const result = await process
+  return { ...process.child, ...result }
+}
+
+const buildNextJsScript = (method) => `
+require('next/dist/build/output/log').${method}('Message for ${method}')
+`
+
+const scenarios = [
+  [
+    'next.js - wait',
+    buildNextJsScript('wait'),
+    { level: 30, name: 'next.js', msg: 'Message for wait', prefix: 'wait' },
+  ],
+  [
+    'next.js - error',
+    buildNextJsScript('error'),
+    { level: 50, name: 'next.js', msg: 'Message for error', prefix: 'error' },
+  ],
+  [
+    'next.js - warn',
+    buildNextJsScript('warn'),
+    { level: 40, name: 'next.js', msg: 'Message for warn', prefix: 'warn' },
+  ],
+  [
+    'next.js - ready',
+    buildNextJsScript('ready'),
+    { level: 30, name: 'next.js', msg: 'Message for ready', prefix: 'ready' },
+  ],
+  [
+    'next.js - info',
+    buildNextJsScript('info'),
+    { level: 30, name: 'next.js', msg: 'Message for info', prefix: 'info' },
+  ],
+  [
+    'next.js - event',
+    buildNextJsScript('event'),
+    { level: 30, name: 'next.js', msg: 'Message for event', prefix: 'event' },
+  ],
+]
+
+describe.each(scenarios)('%s', (_, script, expected) => {
+  it('prints a JSON formatted log to stdout', async () => {
+    const { stdout } = await runScript(script)
+    const log = JSON.parse(stdout)
+
+    expect(log).toMatchObject(expected)
+  })
+
+  it('prints nothing to stderr', async () => {
+    const { stderr } = await runScript(script)
 
     expect(stderr).toBe('')
-    expect(JSON.parse(stdout)).toMatchObject({
-      level: 50,
-      name: 'next.js',
-      msg: 'Something went wrong',
-      prefix: 'error',
-    })
+  })
+
+  it('logs a timestamp close to Date.now()', async () => {
+    const { stdout } = await runScript(script)
+    const log = JSON.parse(stdout)
+    const now = Date.now()
+    // Acceptable margin of quarter of a second each way
+    const acceptableMargin = 250
+
+    expect(typeof log.time).toBe('number')
+    expect(log.time).toBeGreaterThanOrEqual(now - acceptableMargin)
+    expect(log.time).toBeLessThanOrEqual(now + acceptableMargin)
+  })
+
+  it('logs the PID of the process', async () => {
+    const { stdout, pid: expectedPid } = await runScript(script)
+    const { pid } = JSON.parse(stdout)
+
+    expect(typeof pid).toBe('number')
+    expect(pid).toBe(expectedPid)
+  })
+
+  it('logs the hostname of the machine', async () => {
+    const { stdout } = await runScript(script)
+    const { hostname } = JSON.parse(stdout)
+
+    expect(typeof hostname).toBe('string')
+    expect(hostname).toBe(os.hostname())
   })
 })
