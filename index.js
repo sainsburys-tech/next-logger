0e1fabdbbaef147b34f69bf9e5e7dc56e1803bb6
Chris Atkin <atkinchris@gmail.com>
2021-04-30 08:51:37 +0100

    Refactor Next.js patches into their own file
    This refactors the Next.js logger patches into their own file, to be imported in the `index` file. This a precursor to presets.

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const LOGGER_PATH = require('path').join(process.cwd(), 'node_modules', 'next/dist/build/output/log')
-
-// eslint-disable-next-line import/no-dynamic-require
-const nextLogger = require(LOGGER_PATH)
-
-const buildJsonLogger = require('./lib/jsonLogger')
-
-Object.entries(nextLogger).forEach(([key, value]) => {
-  if (typeof value === 'function') {
-    nextLogger[key] = buildJsonLogger(key)
-  }
-})
+require('./lib/patches/next')
