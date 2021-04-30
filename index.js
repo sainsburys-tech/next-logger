fdd94d832d76f1a8cfa5dab9915827ba28b28cde
Chris Atkin <atkinchris@gmail.com>
2021-04-30 08:46:43 +0100

    Refactor logger to lib method

--- index.js
+++ index.js
@@ -2,27 +2,8 @@ const LOGGER_PATH = require('path').join(process.cwd(), 'node_modules', 'next/di
 
 // eslint-disable-next-line import/no-dynamic-require
 const nextLogger = require(LOGGER_PATH)
-const logger = require('pino')({ name: 'next.js' })
 
-const getLogMethod = level => {
-  switch (level) {
-    case 'error':
-      return logger.error.bind(logger, { prefix: level })
-    case 'warn':
-      return logger.warn.bind(logger, { prefix: level })
-    default:
-      return logger.info.bind(logger, { prefix: level })
-  }
-}
-
-const buildJsonLogger = prefix => {
-  const logMethod = getLogMethod(prefix)
-
-  return (...args) => {
-    const message = args.length === 1 ? args[0] : args
-    logMethod(message)
-  }
-}
+const buildJsonLogger = require('./lib/jsonLogger')
 
 Object.entries(nextLogger).forEach(([key, value]) => {
   if (typeof value === 'function') {
