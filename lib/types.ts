ff0d03ae472e795fa0ec0fa15df677169e68f391
devinrhode2 <devinrhode2@gmail.com>
2022-04-20 18:15:35 -0500

    create more "type-safe" console patch with reorderAndTransformArgsForPino

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,30 @@
 /**
+ * Other methods are not supported YET
+ * These methods simply accept strings and objects
+ * Supporting other methods _well_ would be more involved.
+ */
+export type supportedConsoleMethods = keyof Pick<
+  typeof console, // ensure we don't have any typos below, like `warning`
+  | 'error'
+  | 'warn'
+  // | 'dir' // type error with 2nd param `InspectOptions`
+  | 'dirxml'
+  | 'info'
+  | 'log'
+  | 'table'
+  // | 'debug'
+  | 'trace'
+>
+
+export type wrappedPinoLogFn =
+  (typeof console)[supportedConsoleMethods]
+
+export type createPinoCompatibleConsoleFunction = (
+  consoleMethod: supportedConsoleMethods,
+  rawPinoLogFn: import('pino').LogFn,
+) => wrappedPinoLogFn
+
+/**
  * This type isn't all that great.
  * Return type of `[msg: string, ...args: any[]]` is weird.
  * Pino typically expects an object as first param.
