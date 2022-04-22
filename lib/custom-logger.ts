f120729313c08cc8428186c2abf450b1cf16de8f
devinrhode2 <devinrhode2@gmail.com>
2022-04-22 12:21:27 -0500

    fix `any` type for defaultPinoConfig

--- lib/custom-logger.ts
+++ lib/custom-logger.ts
@@ -1,9 +1,23 @@
-import type { defaultPinoConfig } from './defaultPinoConfig'
-import type { P } from 'pino'
+// This is coming out as `any` when using skipLibCheck: true in consuming project
+// Converting to `import { ` (i.e. value import) does not fix the issue.
+// Likely doing full TS conversion will be the best solution.
+// import type { defaultPinoConfig } from './defaultPinoConfig'
+import type { pino } from 'pino'
+
+type _defaultPinoConfigHack = {
+  readonly level: 'debug'
+  readonly hooks: {
+    readonly logMethod: (
+      args: any[],
+      method: pino.LogFn,
+      level: number,
+    ) => void
+  }
+}
 
 export type customLogger = (
-  _defaultPinoConfig: typeof defaultPinoConfig,
-) => P.Logger
+  _defaultPinoConfig: _defaultPinoConfigHack,
+) => pino.Logger
 
 export type NextLoggerConfig = {
   logger?: customLogger
